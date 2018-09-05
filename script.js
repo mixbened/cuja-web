f60232a5a8c28b33b5cf797c668e4b78680bad84
Benedikt Mix <mixbened@gmail.com>
2018-09-05 21:10:40 +0200

    color scheme, footer, hero

---
+++ script.js
@@ -0,0 +1,29 @@
+var formStage = 0;
+var form = document.querySelector('form');
+var mailHTML = "<input class='form-input' name='absender' type='text' placeholder='your contact'>";
+var nameHTML = "<input class='form-input' type='text' placeholder='what is your name?' name='name'>";
+var subjectHTML = "<input class='form-input' type='text' placeholder='what is your Message about?' name='subject'>";
+var textHTML = "<textarea class='form-input' type='text' placeholder='what is on your mind?' name='message'></textarea> <button class='button'>Submit</button>";
+var inputs = [mailHTML, nameHTML, subjectHTML, textHTML]
+
+const nextButton = document.getElementById('next');
+
+function showElement(){
+    if(formStage === 3) {
+        nextButton.remove()
+    }
+    document.querySelector('form').innerHTML = inputs[formStage]
+}
+
+nextButton.addEventListener('click', () => {
+    // there is no 5th Possibility
+    if(formStage < 5){
+        // increase for showing next stage
+        formStage++;
+    }
+    // triggering HTML Switch
+    showElement();
+});
+
+// intitial input Field 
+showElement(); 
