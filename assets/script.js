9c9e2c88c3ca0ff7e01c0c03f376f8d1ba2b780c
Benedikt Mix <mixbened@gmail.com>
2018-10-29 13:40:04 +0100

    responsiveness

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,35 @@
-// FORM PROTOCOLL 
+// FORM PROTOCOLL - Initials
 var formStage = 0;
+var showNav = false;
 var form = document.querySelector('form');
 var mailHTML = "<input class='form-input' name='absender' type='text' placeholder='your contact'>";
 var nameHTML = "<input class='form-input' type='text' placeholder='what is your name?' name='name'>";
 var subjectHTML = "<input class='form-input' type='text' placeholder='what is your Message about?' name='subject'>";
 var textHTML = "<textarea class='form-input' type='text' placeholder='what is on your mind?' name='message'></textarea> <button class='button'>Submit</button>";
-var inputs = [mailHTML, nameHTML, subjectHTML, textHTML]
-
+var inputs = [mailHTML, nameHTML, subjectHTML, textHTML];
+var respNav = $('#respNav')[0];
+var hamburger = $('#hamburger')[0];
 const nextButton = document.getElementById('next');
 
+// NAVIGATION SHOW / HIDE
+hamburger.addEventListener('click', () => {
+    if( respNav.style.display === 'block' ) {
+        respNav.style.display = 'none';
+    } else {
+        respNav.style.display = 'block';
+    }
+    showNav = !showNav;
+})
+
+
+// QUESTION FORM 
 function showElement(){
     if(formStage === 3) {
         nextButton.remove()
     }
     document.querySelector('form').innerHTML = inputs[formStage]
 }
+
 
 nextButton.addEventListener('click', () => {
     // there is no 5th Possibility
@@ -34,7 +49,7 @@ showElement();
 AOS.init();
 
 
-
+// GRIDS ANIMATION
 const grids = $('.gridSystem > div')
 
 grids.mouseover(function(){
